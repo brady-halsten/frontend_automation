f379159fab9340202a264cb08538a439076fade5
FadingOven <bradyhalsten@gmail.com>
2025-07-25 05:56:42 -0700

    implemented validation for email format as well as checks for existing users

--- registration/app1/views.py
+++ registration/app1/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from email_validator import validate_email, EmailNotValidError
 # Create your views here.
 @login_required(login_url='login')
 def HomePage(request):
@@ -10,18 +11,29 @@ def HomePage(request):
 def SignupPage(request):
     if request.method=='POST':
         uname=request.POST.get('username')
+        if User.objects.filter(username=uname).exists():
+            return HttpResponse("Username already taken.")
+
         email=request.POST.get('email')
+        try:
+            email = validate_email(email, check_deliverability=True)
+            email = email.normalized
+        except EmailNotValidError as e:
+            return HttpResponse(f'Invalid Email Address: {str(e)}')
+        if User.objects.filter(email=email).exists():
+            return HttpResponse("An account with this email address already exists.")
+
         pass1=request.POST.get('password1')
         pass2=request.POST.get('password2')
 
         if pass1!=pass2:
-            return HttpResponse("Your password and confrom password are not Same!!")
+            return HttpResponse("Your password and confirm password are not Same!!")
         else:
 
             my_user=User.objects.create_user(uname,email,pass1)
             my_user.save()
             return redirect('login')
-        
+
 
 
 
